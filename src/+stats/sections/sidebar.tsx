0f84c412e32e2a44e522a79d3a3dd5ef33f94172
Pedro Gutiérrez <pedrogp9693@gmail.com>
2021-11-03 01:23:01 +0100

    Allow to configure time period and asset in sidebar

--- src/+stats/sections/sidebar.tsx
+++ src/+stats/sections/sidebar.tsx
@@ -1,18 +1,35 @@
 import React, { useCallback } from 'react'
-import { useStoreState } from 'src/shared/store'
+import { useStoreState, useStoreActions } from 'src/shared/store'
 
-import { UiText } from 'ui'
+import { UiText, UiSelector } from 'ui'
 
 import styles from './sidebar.module.scss'
 
 export const Sidebar = () => {
   const crypto = useStoreState((state) => state.crypto)
   const cryptoList = useStoreState((state) => state.cryptoList)
+  const timePeriod = useStoreState((state) => state.timePeriod)
+
+  const changeCrypto = useStoreActions((actions) => actions.changeCrypto)
+  const changeTimePeriod = useStoreActions((actions) => actions.changeTimePeriod)
 
   return (
     <>
       <UiText type="h1" className={styles.heading}>Massari Stats</UiText>
-      <UiText type="h2">{crypto} metrics</UiText>
+
+      <UiSelector
+        title="Selected asset"
+        options={cryptoList}
+        value={crypto}
+        onChange={changeCrypto}
+      />
+
+      <UiSelector
+        title="Time period"
+        options={['week', 'month']}
+        value={timePeriod}
+        onChange={changeTimePeriod}
+      />
     </>
   )
 }
